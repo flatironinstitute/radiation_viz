d3001f5f6521f065602ed41effbcefa63b76bf22
Aaron Watters <awatters@simonsfoundation.org>
2020-05-01 11:04:00 -0400

    don't request exclusive in batch job

--- radiation_viz/capture.sh
+++ radiation_viz/capture.sh
@@ -2,15 +2,21 @@
 
 # request a single gpu node for the run.
 
-#SBATCH -N1 --exclusive --gres=gpu:1 -p gpu bash
+#SBATCH -N1 --gres=gpu:1 -p gpu
 
 # module load slurm
-# srun -N1 --exclusive --gres=gpu:1 -p gpu bash capture.sh
+# srun -N1 --gres=gpu:1 -p gpu bash capture.sh
+
+# to cancel
+# $ squeue | grep awatters
+#            554219       gpu capture. awatters  R    2:18:07      1 workergpu05
+# $ scancel 554219
 
 source activate nodetest
 python -m radiation_viz.capture_images \
      --to_directory /mnt/ceph/users/awatters/images \
      --http_directory /mnt/ceph/users/awatters/viz \
      --node_directory ~/repos/radiation_viz/image_capturer \
+     --settings_path ~/repos/radiation_viz/radiation_viz/example_camera_settings.json \
      --limit 3000 > /mnt/ceph/users/awatters/logs/capture.log
 
